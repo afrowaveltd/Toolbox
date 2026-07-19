ee3787d5b537258a38f487895941fd6bb32f62a9
Afrowave Community <65894408+afrowaveltd@users.noreply.github.com>
2026-07-19 20:44:32 +0200

    Test case-insensitive documentation key suffix collisions

---
+++ Toolroom/WhenItFails/Setter.Tests/Planning/WhenItFailsDocumentationKeySuggesterCaseInsensitiveSuffixCollisionTests.cs
@@ -0,0 +1,82 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Afrowave.Toolbox.Essentials.Results;
+using Afrowave.Toolbox.Toolroom.WhenItFails.Setter.Planning;
+using Afrowave.Toolbox.Toolroom.WhenItFails.Setter.Tests.Infrastructure;
+using Afrowave.Toolbox.WhenItFails.Configuration;
+
+namespace Afrowave.Toolbox.Toolroom.WhenItFails.Setter.Tests.Planning;
+
+public sealed class WhenItFailsDocumentationKeySuggesterCaseInsensitiveSuffixCollisionTests
+{
+    [Fact]
+    public async Task SuggestAsync_WhenUppercaseSecondSuffixExists_ReturnsThirdSuffix()
+    {
+        using TemporaryWhenItFailsWorkspace workspace =
+            await TemporaryWhenItFailsWorkspace.CreateInitializedAsync();
+        JsonsOptions options = WhenItFailsWorkspacePathResolver.ResolveJsonsOptions(
+            workspace.ProjectRootPath);
+        WhenItFailsDocumentationKeySuggester suggester = new();
+
+        Response<DocumentationKeySuggestion> initialResponse = await suggester.SuggestAsync(
+            options,
+            "NETWORK",
+            "Case insensitive suffix sample");
+
+        Assert.True(initialResponse.IsSuccess);
+        DocumentationKeySuggestion initialSuggestion =
+            Assert.IsType<DocumentationKeySuggestion>(initialResponse.Data);
+
+        string errorCatalogPath = Path.Combine(
+            options.RootDirectory,
+            options.PackageDirectoryName,
+            options.ErrorCatalogFileName);
+        JsonObject catalog = Assert.IsType<JsonObject>(
+            JsonNode.Parse(await File.ReadAllTextAsync(errorCatalogPath)));
+        JsonArray errors = Assert.IsType<JsonArray>(catalog["errors"]);
+        JsonObject template = Assert.IsType<JsonObject>(errors[0]?.DeepClone());
+
+        errors.Add(CreateCollision(
+            template,
+            "AFW_TEST_9911",
+            991001,
+            "CASEINSENSITIVESUFFIXBASE",
+            initialSuggestion.DocumentationKey));
+        errors.Add(CreateCollision(
+            template,
+            "AFW_TEST_9912",
+            991002,
+            "CASEINSENSITIVESUFFIXTWO",
+            $"{initialSuggestion.DocumentationKey}-2".ToUpperInvariant()));
+
+        await File.WriteAllTextAsync(
+            errorCatalogPath,
+            catalog.ToJsonString(new JsonSerializerOptions { WriteIndented = true }));
+
+        Response<DocumentationKeySuggestion> response = await suggester.SuggestAsync(
+            options,
+            "NETWORK",
+            "Case insensitive suffix sample");
+
+        Assert.True(response.IsSuccess);
+        DocumentationKeySuggestion suggestion =
+            Assert.IsType<DocumentationKeySuggestion>(response.Data);
+        Assert.Equal($"{initialSuggestion.DocumentationKey}-3", suggestion.DocumentationKey);
+    }
+
+    private static JsonObject CreateCollision(
+        JsonObject template,
+        string id,
+        int code,
+        string name,
+        string documentationKey)
+    {
+        JsonObject collision = Assert.IsType<JsonObject>(template.DeepClone());
+        collision["id"] = id;
+        collision["code"] = code;
+        collision["name"] = name;
+        collision["title"] = "Case insensitive suffix sample";
+        collision["documentationKey"] = documentationKey;
+        return collision;
+    }
+}
