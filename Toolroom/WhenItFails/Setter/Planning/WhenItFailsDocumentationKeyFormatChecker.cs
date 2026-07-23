57c178b9e1491d22e69b2014e3f862c1eea212b2
Afrowave Community <65894408+afrowaveltd@users.noreply.github.com>
2026-07-23 22:42:42 +0200

    Use library documentation key format checker in setter

--- Toolroom/WhenItFails/Setter/Planning/WhenItFailsDocumentationKeyFormatChecker.cs
+++ Toolroom/WhenItFails/Setter/Planning/WhenItFailsDocumentationKeyFormatChecker.cs
@@ -1,4 +1,5 @@
 using Afrowave.Toolbox.WhenItFails.Definitions;
+using Afrowave.Toolbox.WhenItFails.Documentation;
 
 namespace Afrowave.Toolbox.Toolroom.WhenItFails.Setter.Planning;
 
@@ -18,7 +19,7 @@ internal sealed class WhenItFailsDocumentationKeyFormatChecker
 
         InvalidDocumentationKeyFormat[] invalidKeys = errors
             .Where(error => !string.IsNullOrWhiteSpace(error.DocumentationKey))
-            .Where(error => !IsCanonical(error.DocumentationKey!))
+            .Where(error => !DocumentationKeyFormat.IsCanonical(error.DocumentationKey!))
             .OrderBy(error => error.Code)
             .ThenBy(error => error.Id, StringComparer.OrdinalIgnoreCase)
             .Select(error => new InvalidDocumentationKeyFormat(
@@ -38,57 +39,7 @@ internal sealed class WhenItFailsDocumentationKeyFormatChecker
     /// </summary>
     public static bool IsCanonical(string documentationKey)
     {
-        ArgumentNullException.ThrowIfNull(documentationKey);
-
-        if (!string.Equals(
-                documentationKey,
-                documentationKey.Trim(),
-                StringComparison.Ordinal))
-        {
-            return false;
-        }
-
-        string[] segments = documentationKey.Split('/');
-        if (segments.Length < 2)
-        {
-            return false;
-        }
-
-        return segments.All(IsCanonicalSegment);
-    }
-
-    private static bool IsCanonicalSegment(string segment)
-    {
-        if (segment.Length == 0 || segment[0] == '-' || segment[^1] == '-')
-        {
-            return false;
-        }
-
-        bool previousWasHyphen = false;
-
-        foreach (char character in segment)
-        {
-            if (character == '-')
-            {
-                if (previousWasHyphen)
-                {
-                    return false;
-                }
-
-                previousWasHyphen = true;
-                continue;
-            }
-
-            if (character is not (>= 'a' and <= 'z') &&
-                character is not (>= '0' and <= '9'))
-            {
-                return false;
-            }
-
-            previousWasHyphen = false;
-        }
-
-        return true;
+        return DocumentationKeyFormat.IsCanonical(documentationKey);
     }
 }
 
