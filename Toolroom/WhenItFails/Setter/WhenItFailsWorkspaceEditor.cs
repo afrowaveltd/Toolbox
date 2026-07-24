32681bfcd4d6beeb3cf6cad24943377c64c4c075
Afrowave Community <65894408+afrowaveltd@users.noreply.github.com>
2026-07-24 21:53:48 +0200

    Use library documentation key format in workspace editor

--- Toolroom/WhenItFails/Setter/WhenItFailsWorkspaceEditor.cs
+++ Toolroom/WhenItFails/Setter/WhenItFailsWorkspaceEditor.cs
@@ -2,6 +2,7 @@ using Afrowave.Toolbox.Essentials.Results;
 using Afrowave.Toolbox.Toolroom.WhenItFails.Setter.Planning;
 using Afrowave.Toolbox.WhenItFails.Configuration;
 using Afrowave.Toolbox.WhenItFails.Definitions;
+using Afrowave.Toolbox.WhenItFails.Documentation;
 using Afrowave.Toolbox.WhenItFails.Loading;
 using Afrowave.Toolbox.WhenItFails.Normalization;
 using Afrowave.Toolbox.WhenItFails.Validation;
@@ -106,7 +107,7 @@ internal sealed class WhenItFailsWorkspaceEditor
        CancellationToken cancellationToken = default)
    {
       if (!string.IsNullOrWhiteSpace(newDocumentationKey) &&
-          !WhenItFailsDocumentationKeyFormatChecker.IsCanonical(newDocumentationKey))
+          !DocumentationKeyFormat.IsCanonical(newDocumentationKey))
       {
          return Response<ErrorDefinition>.Invalid(
              code: "InvalidDocumentationKeyFormat",
