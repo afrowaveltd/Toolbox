2e78f0030ac0c9514a0d2a3fd893d5042d2ccc2f
Afrowave Community <65894408+afrowaveltd@users.noreply.github.com>
2026-07-25 21:30:15 +0200

    Serialize explain profile console tests

--- Toolroom/WhenItFails/Setter.Tests/Commands/ExplainProfileCommandTests.cs
+++ Toolroom/WhenItFails/Setter.Tests/Commands/ExplainProfileCommandTests.cs
@@ -7,6 +7,7 @@ using Afrowave.Toolbox.WhenItFails.Normalization;
 
 namespace Afrowave.Toolbox.Toolroom.WhenItFails.Setter.Tests.Commands;
 
+[Collection(ConsoleOutputTestCollection.Name)]
 public sealed class ExplainProfileCommandTests
 {
     [Fact]
