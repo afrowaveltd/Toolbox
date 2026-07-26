670f83d6b28875d5a9ce2b727b67f083a639f49c
Afrowave Community <65894408+afrowaveltd@users.noreply.github.com>
2026-07-26 12:17:34 +0200

    Test invalid documentation key error id

---
+++ Toolroom/WhenItFails/Setter.Tests/Planning/InvalidDocumentationKeyFormatTests.cs
@@ -0,0 +1,22 @@
+using Afrowave.Toolbox.Toolroom.WhenItFails.Setter.Planning;
+
+namespace Afrowave.Toolbox.Toolroom.WhenItFails.Setter.Tests.Planning;
+
+public sealed class InvalidDocumentationKeyFormatTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Constructor_WithMissingErrorId_ThrowsArgumentException(string? errorId)
+    {
+        ArgumentException exception = Assert.ThrowsAny<ArgumentException>(
+            () => new InvalidDocumentationKeyFormat(
+                ErrorId: errorId!,
+                ErrorCode: 1,
+                ErrorName: "Error one",
+                DocumentationKey: "General/Invalid_Key"));
+
+        Assert.Equal("ErrorId", exception.ParamName);
+    }
+}
