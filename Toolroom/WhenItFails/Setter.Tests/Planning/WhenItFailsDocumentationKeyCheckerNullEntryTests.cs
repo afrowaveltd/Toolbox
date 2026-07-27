1b1c23663c9df0a0f0c41abd44a06c0bf73b8b42
Afrowave Community <65894408+afrowaveltd@users.noreply.github.com>
2026-07-27 08:05:37 +0200

    Test null documentation key catalog entry

---
+++ Toolroom/WhenItFails/Setter.Tests/Planning/WhenItFailsDocumentationKeyCheckerNullEntryTests.cs
@@ -0,0 +1,21 @@
+using Afrowave.Toolbox.Toolroom.WhenItFails.Setter.Planning;
+using Afrowave.Toolbox.WhenItFails.Definitions;
+
+namespace Afrowave.Toolbox.Toolroom.WhenItFails.Setter.Tests.Planning;
+
+public sealed class WhenItFailsDocumentationKeyCheckerNullEntryTests
+{
+    [Fact]
+    public void Check_WithNullErrorEntry_ThrowsArgumentException()
+    {
+        ErrorCatalogDocument catalog = new()
+        {
+            Errors = [null!]
+        };
+
+        ArgumentException exception = Assert.Throws<ArgumentException>(
+            () => new WhenItFailsDocumentationKeyChecker().Check(catalog));
+
+        Assert.Equal("catalog", exception.ParamName);
+    }
+}
